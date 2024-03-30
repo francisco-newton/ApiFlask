9853678f5c12e9549fbcea9f29181c85ac903d55
francisco-newton <newton.silvanunes@hotmail.com>
2024-03-30 14:29:59 -0300

    feat: rotas da api

---
+++ main.py
@@ -0,0 +1,121 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Dados de exemplo
+"""
+{
+   'usuario' : 'marcilio',
+   'nome' : 'Marcilio F Oliveira',
+   'id' : 1,
+   'carrinho' : {
+        'total' : 42,53
+        'produtos' : [
+             {
+                  'codigo' : 1,
+                  'nome' : 'película de celular',
+                  'valor': 30,00
+              },
+              {
+                  'codigo' : 22,
+                  'nome' : 'caneta de quadro branco',
+                  'valor' : 12,53
+              }
+         ]
+    }
+}
+"""
+
+usuariosBanco = [{
+   'usuario' : 'marcilio',
+   'nome' : 'Marcilio F Oliveira',
+   'id' : 1,
+   'carrinho' : {}
+}]
+
+
+@app.route('/usuarios/todos', methods=['GET'])
+def get_usuarios():
+    return {'Resposta' : usuariosBanco}
+
+@app.route('/usuarios/novo', methods=['POST'])
+def new_usuario():
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    novoUsuario = {
+        'usuario' : data.get('usuario'),
+        'nome' : data.get('nome'),
+        'id' : usuariosBanco[-1]["id"] + 1,
+        'carrinho' : {}
+    }
+
+    usuariosBanco.append(novoUsuario)
+
+    return {"Resposta": 'Usuário criado com sucesso!'}
+
+@app.route('/usuarios/atualizar/<int:id>', methods=['PUT'])
+def update_usuario(id):
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    usuariosBanco[id-1]['nome'] = data.get('nome')
+    return {"Resposta": 'Usuário atualizado com sucesso!'}
+
+@app.route('/carrinho/<int:id>', methods=['GET'])
+def get_carrinho(id):
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    return {'Resposta' : usuariosBanco[id-1]['carrinho']}
+
+@app.route('/carrinho/adicionar/<int:id>', methods=['POST'])
+def add_produto(id):
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    novoProduto = {
+        'codigo' : data.get('codigo'),
+        'nome' : data.get('nome'),
+        'valor' : data.get('valor')
+    }
+
+    usuariosBanco[id-1]['carrinho']['produtos'].append(novoProduto)
+    usuariosBanco[id-1]['carrinho']['total'] += novoProduto['valor']
+
+    return {"Resposta": 'Produto adicionado com sucesso!'}
+
+@app.route('/carrinho/remover/<int:id>', methods=['DELETE'])
+def remove_produto(id):
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    usuariosBanco[id-1]['carrinho']['total'] -= data.get('valor')
+    usuariosBanco[id-1]['carrinho']['produtos'].remove(data.get('produto'))
+
+    return {"Resposta": 'Produto removido com sucesso!'}
+
+@app.route('/carrinhos/total', methods=['GET'])
+def get_total():
+    if request.json:
+        data = request.json
+    else:
+        return {'Resposta': 'Formato de dados inválido'}
+
+    total = 0
+    for usuario in usuariosBanco:
+        total += usuario['carrinho']['total']
+    return {'Resposta' : total}
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
